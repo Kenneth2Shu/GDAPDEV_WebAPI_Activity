07708413ef8e4e74b2062518548ad037cf8cc3cc
Kenneth2Shu <darkryder42@gmail.com>
2024-07-23 13:40:52 +0800

    minion thingy

--- Assets/Scripts/Hearthstone/Card.cs
+++ Assets/Scripts/Hearthstone/Card.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 
+[JsonObject(MemberSerialization.OptIn)]
 public class Card : MonoBehaviour {
     [JsonProperty("code")]
     public string Code { get; set; }
